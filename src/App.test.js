7e842528351f21eb7caeb828ce88cad18fd7a8f5
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-25 12:32:35 +0100

    Refactors to use beforeEach() to DRY up test

--- src/App.test.js
+++ src/App.test.js
@@ -4,13 +4,17 @@ import App from './App';
 import Calculator from './Calculator';
 
 describe('App', () => {
+
+  let  wrapper;
+
+  beforeEach(() => wrapper = shallow(<App />));
+
+
   it('should render a <div />', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('div').length).toEqual(1);
   });
 
   it('renders an instance of the Calculator component', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true);
   });
 });
