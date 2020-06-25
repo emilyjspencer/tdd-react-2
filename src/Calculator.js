a6191cccf34402641877c13a14bfec1c9a381f46
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-25 12:39:35 +0100

    Adds third passing test. Makes Calculator a classical component

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Calculator = () => {
+class Calculator extends Component {
+  state = {
+      displayValue: '0',
+      numbers: [],
+      operators: [],
+      selectedOperator: '',
+      storedValue: ''
+  }
 
+  render() {
+      return(
+          <div className="calculator-container">
+           <h1>Hi - I am a Calculator</h1>
+          </div>
+      );
+  }
 }
 
 
