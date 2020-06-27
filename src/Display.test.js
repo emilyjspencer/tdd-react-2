2891331b8139c05fe9c1535e367e6881481760cc
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-27 22:46:16 +0100

    Adds passing snapshot test for the Display component

--- src/Display.test.js
+++ src/Display.test.js
@@ -8,6 +8,10 @@ let wrapper;
 
 beforeEach(() => wrapper = shallow(<Display displayValue={''} />));
 
+it('renders correctly', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
 it('should render a <div />', () => {
   expect(wrapper.find('div').length).toEqual(1)
 });
